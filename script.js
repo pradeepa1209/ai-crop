3512b58cecedd5ac32a28faf439e43acc226ef2b
nivishna04 <nivishna689@gmail.com>
2026-03-28 10:40:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function showResult(){
 
-  let rainfall = document.getElementById("rainfall").value;
-  let temperature = document.getElementById("temperature").value;
-  let area = document.getElementById("area").value;
+let rainfall = document.getElementById("rainfall").value;
+let temperature = document.getElementById("temperature").value;
 
-  if(rainfall === "" || temperature === "" || area === ""){
-    document.getElementById("result").innerText = "Please fill all fields!";
-  } else {
-    document.getElementById("result").innerText =
-      "Predicted Yield: 3.2 tons/hectare (Demo)";
-  }
+if(rainfall === "" || temperature === ""){
+document.getElementById("result").innerText = "Please fill all fields!";
+}
+else{
+document.getElementById("result").innerText =
+"Predicted Yield: 3.2 tons/hectare (Demo)";
+}
 }
