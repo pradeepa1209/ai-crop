9a15f1c7f2bea0f9b9a3a4618b4647a7bac210aa
nivishna04 <nivishna689@gmail.com>
2026-03-28 10:23:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,39 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
 
+app = Flask(__name__)
+
+# Load model
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/input")
+def input_page():
+    return render_template("input.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        values = [
+            float(request.form["N"]),
+            float(request.form["P"]),
+            float(request.form["K"]),
+            float(request.form["temperature"]),
+            float(request.form["humidity"]),
+            float(request.form["ph"]),
+            float(request.form["rainfall"])
+        ]
+
+        prediction = model.predict([values])[0]
+
+        return render_template("input.html", result=prediction)
+
+    except:
+        return render_template("input.html", result="❌ Please enter valid values!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
