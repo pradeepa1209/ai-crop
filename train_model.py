6f6f08fc98558bdb6a83053c9f66345a9c4a980d
nivishna04 <nivishna689@gmail.com>
2026-03-28 10:41:13 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,18 +2,14 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
-# Load dataset
 data = pd.read_csv("dataset.csv")
 
-# Features & Label
 X = data.drop("label", axis=1)
 y = data["label"]
 
-# Train model
 model = RandomForestClassifier()
 model.fit(X, y)
 
-# Save model
 pickle.dump(model, open("model.pkl", "wb"))
 
-print("✅ Model trained successfully!")
+print("Model trained successfully")
